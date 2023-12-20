6e02d7de9184f86f5f84c5e5424f594c6208c103
ellian-abe <ellian_abe@outlook.com>
2023-12-20 12:00:00 -0300

    remove tmp files (lock-flag)

--- ppt_to_pdf.py
+++ ppt_to_pdf.py
@@ -1,12 +1,18 @@
 from typing import Literal
 import subprocess
 import os
+import platform
 
 
 def convert(input_ppt, output_dir, method: Literal['libreoffice', 'powerpoint'] = 'libreoffice'):
     try:
         if method == 'libreoffice':
-            subprocess.run(['libreoffice', '--headless', '--invisible',
+            if platform.system() == "Windows":
+                libreoffice = r"C:\Program Files\LibreOffice\program\soffice.exe"
+            else:
+                libreoffice == 'libreoffice'
+                
+            subprocess.run([libreoffice, '--headless', '--invisible',
                             '--convert-to', 'pdf', '--outdir',
                             os.path.dirname(output_dir), input_ppt],
                            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
